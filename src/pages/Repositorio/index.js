f62f67afa1a598e1d39f04eb9e6c3a85a1329868
Jefferson Sousa de Paiva <metaljefferson11@hotmail.com>
2023-07-25 11:12:43 -0300

    Projeto Finalizado

---
+++ src/pages/Repositorio/index.js
@@ -0,0 +1,141 @@
+import { useParams } from "react-router-dom";
+import {
+  Container,
+  Owner,
+  Loading,
+  BackButton,
+  IssuesList,
+  PageActions,
+  FilterList,
+} from "./styles";
+import { FaArrowLeft } from "react-icons/fa";
+import api from "../../services/api";
+import React, { useEffect, useState } from "react";
+
+export default function Repositorio() {
+  const [repo, setRepo] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [filters, setFilters] = useState([
+    { state: "all", label: "Todas", active: true },
+    { state: "open", label: "Abertas", active: false },
+    { state: "closed", label: "fechadas", active: false },
+  ]);
+  const [filterIndex, setfilterIndex] = useState(0);
+
+  const { repositorio } = useParams();
+
+  useEffect(() => {
+    async function load() {
+      const nomeRepo = decodeURIComponent(repositorio);
+
+      const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${nomeRepo}`),
+        api.get(`/repos/${nomeRepo}/issues`, {
+          params: {
+            state: filters.find((f) => f.active).state,
+            per_page: 5,
+          },
+        }),
+      ]);
+
+      setRepo(repositorioData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+    }
+
+    load();
+  }, [repositorio,filters]);
+
+  useEffect(() => {
+    async function loadIssue() {
+      const nomeRepo = decodeURIComponent(repositorio);
+
+      const response = await api.get(`/repos/${nomeRepo}/issues`, {
+        params: {
+          state: filters[filterIndex].state,
+          page: page,
+          per_page: 5,
+        },
+      });
+      setIssues(response.data);
+    }
+    loadIssue();
+  }, [filterIndex,filters,repositorio, page]);
+
+  function handlePage(action) {
+    setPage(action === "back" ? page - 1 : page + 1);
+  }
+
+  function handleFilter(index) {
+    setfilterIndex(index);
+  }
+
+  if (loading) {
+    return (
+      <Loading>
+        <h1>Carregando...</h1>
+      </Loading>
+    );
+  }
+
+  return (
+    <Container>
+      <BackButton to="/">
+        <FaArrowLeft color="#000" size={30} />
+      </BackButton>
+      <Owner>
+        {repo.owner && (
+          <>
+            <img src={repo.owner.avatar_url} alt={repo.owner.login} />
+            <h1>{repo.name}</h1>
+            <p>{repo.description}</p>
+          </>
+        )}
+      </Owner>
+
+      <FilterList active={filterIndex}>
+        {filters.map((filter, index) => (
+          <button
+            type="button"
+            key={filter.label}
+            onClick={() => handleFilter(index)}
+          >
+            {filter.label}
+          </button>
+        ))}
+      </FilterList>
+
+      <IssuesList>
+        {issues.map((issue) => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+
+                {issue.labels.map((label) => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssuesList>
+      <PageActions>
+        <button
+          type="button"
+          onClick={() => handlePage("back")}
+          disabled={page < 2}
+        >
+          Voltar
+        </button>
+        <button type="button" onClick={() => handlePage("next")}>
+          Proxima
+        </button>
+      </PageActions>
+    </Container>
+  );
+}
